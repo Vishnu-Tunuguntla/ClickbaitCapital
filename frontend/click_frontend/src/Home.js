59714f57899daa3958fc5e04e5bcf1147d09768b
Vishnu Desktop <vtunuguntla022@gmail.com>
2024-06-17 13:23:08 -0400

    changed slider min to 0

--- frontend/click_frontend/src/Home.js
+++ frontend/click_frontend/src/Home.js
@@ -41,7 +41,7 @@ function Home({ topStocks, setTopStocks }) {
           <div className="slider-label">Reddit: <span>{reddit}</span></div>
           <input
             type="range"
-            min="1"
+            min="0"
             max="10"
             value={reddit}
             onChange={(e) => setReddit(e.target.value)}
@@ -51,7 +51,7 @@ function Home({ topStocks, setTopStocks }) {
           <div className="slider-label">Instagram: <span>{instagram}</span></div>
           <input
             type="range"
-            min="1"
+            min="0"
             max="10"
             value={instagram}
             onChange={(e) => setInstagram(e.target.value)}
@@ -61,7 +61,7 @@ function Home({ topStocks, setTopStocks }) {
           <div className="slider-label">Facebook: <span>{facebook}</span></div>
           <input
             type="range"
-            min="1"
+            min="0"
             max="10"
             value={facebook}
             onChange={(e) => setFacebook(e.target.value)}
